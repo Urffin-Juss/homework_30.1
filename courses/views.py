ed8dba7e6bbfd074125728a9dbd1ae2b40b117e3
Urffin-Juss <im@maxsuns.ru>
2026-01-27 16:57:11 +0300

    add view courses

--- courses/views.py
+++ courses/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from rest_framework import generics
+from .models import Course, Lesson
+from .serializers import CourseSerializer, LessonSerializer
 
-from courses.models import Course
+from courses.models import Course, Lesson
+
 
 
 class CourseViewSet(viewsets.ModelViewSet):
+
     queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class LessonListCreatedView(generics.ListCreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class LessonRetrieveUpdatedView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+
 
 
 
