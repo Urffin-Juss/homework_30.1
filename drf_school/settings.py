aaf3d354ac76cd1e570f1b5c92167cf8142a25bb
Urffin-Juss <im@maxsuns.ru>
2026-01-27 14:20:40 +0300

    start config settings

--- drf_school/settings.py
+++ drf_school/settings.py
@@ -1,5 +1,10 @@
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!5)nlk0u0_ntq8y)4e2-61&87lg4(0&ynl9dvjr&i89z^3k&ee'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
