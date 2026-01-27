e87d717eab776b525e77d12664ec9afa04833a22
Urffin-Juss <im@maxsuns.ru>
2026-01-27 15:59:24 +0300

    add course\lesson models

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
+class Course(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    preview = models.ImageField(upload_to='courses/%Y/%m')
+    description = models.TextField(max_length=500)
+
+
+    class Meta:
+        db_table = 'course'
+        ordering = ['title']
+
+
+class Lesson(models.Model):
+    lesson = models.ForeignKey('Lesson', on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    preview = models.ImageField(upload_to='lessons/%Y/%m')
+    description = models.TextField(max_length=500)
+
+
+    class Meta:
+        db_table = 'lesson'
+        ordering = ['title']
+
+
 # Create your models here.
