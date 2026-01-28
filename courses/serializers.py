7a4ce462c8f25c34f288c835a825b2a58efe8773
Urffin-Juss <im@maxsuns.ru>
2026-01-28 18:04:33 +0300

    rewrite serial

--- courses/serializers.py
+++ courses/serializers.py
@@ -10,9 +10,18 @@ class LessonSerializer(serializers.ModelSerializer):
 
 class CourseSerializer(serializers.ModelSerializer):
 
+    lesson_count = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Course
+        fields = '__all__'
     lessons = LessonSerializer(many=True, read_only=True)
     lessons_count = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Course
         fields = '__all__'
+
+
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
