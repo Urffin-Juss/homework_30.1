d5286136d3dc7104eea3b1dded1a37d9cfaa36e4
Urffin-Juss <im@maxsuns.ru>
2026-01-28 18:15:27 +0300

    add new class

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from phonenumber_field.modelfields  import PhoneNumberField
+from django.conf import settings
 
 
 
@@ -53,4 +54,58 @@ class User(AbstractUser):
 
 
 
-# Create your models here.
+class Payment(models.Model):
+    """Payment model"""
+
+    PAYMENT_METHOD_CHOICES = [
+        ('cash', 'cash'),
+        ('transfer', 'transfer'),
+
+    ]
+
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+        related_name='payments',
+        verbose_name='user',
+    )
+    payment_date = models.DateField(verbose_name='payment date')
+    paid_course = models.ForeignKey(
+        'courses.Course',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name='paid course',
+    )
+
+    paid_lesson = models.ForeignKey(
+        'lessons.Lesson',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name='paid lesson',
+
+    )
+
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name='amount',
+    )
+
+    payment_method = models.CharField(
+        max_length=20,
+        choices=PAYMENT_METHOD_CHOICES,
+        verbose_name='payment method',
+    )
+
+    def __str__(self):
+        return f"Payment {self.id} from {self.user.email} - {self.amount}"
+
+    class Meta:
+        verbose_name = 'payment'
+        verbose_name_plural = 'payments'
+        ordering = ('-payment_date',)
+
+
+
