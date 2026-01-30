21bae96220c3456406a21697eb0967286c4d8662
Urffin-Juss <im@maxsuns.ru>
2026-01-30 12:17:11 +0300

    add urls in drf

--- drf_school/urls.py
+++ drf_school/urls.py
@@ -2,12 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('courses.urls')),
     path('api/', include('users.urls')),
+
+    # JWT
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
 
