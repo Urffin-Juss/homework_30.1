c0f5d4f4b7fa57723046c2b3abe304d282359848
Urffin-Juss <im@maxsuns.ru>
2026-01-31 16:17:24 +0300

    befor migrrate

--- courses/serializers.py
+++ courses/serializers.py
@@ -3,12 +3,16 @@ from courses.models import Course, Lesson
 
 
 class LessonSerializer(serializers.ModelSerializer):
+
+
     class Meta:
         model = Lesson
         fields = '__all__'
+        read_only_fields = ('owner',)
 
 
 class CourseSerializer(serializers.ModelSerializer):
+
     lessons = LessonSerializer(many=True, read_only=True)
     lessons_count = serializers.SerializerMethodField(read_only=True)
 
@@ -16,6 +20,7 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = '__all__'
+        read_only_fields = ('owner',)
 
 
     def get_lessons_count(self, obj):
