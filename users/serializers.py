f93150c3a936e93e803b74ff8e090c90f254bfa5
Urffin-Juss <im@maxsuns.ru>
2026-02-02 10:53:58 +0300

    rewrite urls

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,9 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Payment, User
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,16 +12,27 @@ class PaymentSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, required=False)
 
     class Meta:
         model = User
-        fields = "__all__"
+        fields = ("id", "email", "password", "first_name", "last_name", "phone_number", "city", "avatar")
 
     def create(self, validated_data):
-        password = validated_data.pop("password")
+        password = validated_data.pop("password", None)
         user = User(**validated_data)
-        user.set_password(password)
+        if password:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
         user.save()
         return user
 
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PaymentViewSet, RegisterView
+
+from courses.views import CourseViewSet
+from .views import PaymentViewSet, RegisterView, UserViewSet
 
 router = DefaultRouter()
-router.register('payments', PaymentViewSet, basename='payment')
+router.register(r'users', UserViewSet, basename='user')
 
-urlpatterns = [
-    path('', include(router.urls)),
-    path('register/', RegisterView.as_view(), name='register'),
 
+urlpatterns = router.urls
 
 
 
-]
