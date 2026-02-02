443007dad0c5aa20cf11d52a1c02858668210ea1
Urffin-Juss <im@maxsuns.ru>
2026-02-02 11:40:42 +0300

    add imports

--- drf_school/urls.py
+++ drf_school/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
